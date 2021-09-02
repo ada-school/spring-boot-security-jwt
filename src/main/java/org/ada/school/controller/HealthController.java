bc03961a010eb81369b10b4201ee2b3095dbf784
sancarbar <sancarbar@gmail.com>
2021-09-01 22:20:11 -0500

    Added HealthController.

---
+++ src/main/java/org/ada/school/controller/HealthController.java
@@ -0,0 +1,17 @@
+package org.ada.school.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( "v1/health" )
+public class HealthController
+{
+
+    @GetMapping
+    public String all()
+    {
+        return "API Working OK!";
+    }
+}
