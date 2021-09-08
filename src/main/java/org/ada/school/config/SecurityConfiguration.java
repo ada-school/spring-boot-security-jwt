4e70065a02c4fa5a7577af96f78949119fe7625b
Guillermo Castro <guillermo.castro@mail.escuelaing.edu.co>
2021-09-08 02:45:52 -0500

    Final Part 3 without tests

--- src/main/java/org/ada/school/config/SecurityConfiguration.java
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -30,6 +30,19 @@ public class SecurityConfiguration
                 "/v1/auth" ).permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(
                 SessionCreationPolicy.STATELESS );
     }
-}
 
+//    @Override
+//    protected void configure( HttpSecurity http )
+//            throws Exception
+//    {
+//        http.cors().and().csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers( HttpMethod.GET, "/v1/health" ).permitAll()
+//                .antMatchers( HttpMethod.POST,"/v1/auth" ).permitAll()
+//                .antMatchers( HttpMethod.POST,"/v1/user" ).permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS );
+//    }
+}
 
