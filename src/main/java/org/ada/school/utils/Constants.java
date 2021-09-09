65c997dc76af7010ca4b66f5bb6863deda85fae0
PaulaSanchez810 <paula.guevara-s@mail.escuelaing.edu.co>
2021-09-09 11:07:07 -0500

    class rest controller and package utils

---
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -0,0 +1,8 @@
+package org.ada.school.utils;
+
+public class Constants {
+
+     public static final String CLAIMS_ROLES_KEY = "roles";
+
+     public static final int TOKEN_DURATION_MINUTES = 4;
+}
