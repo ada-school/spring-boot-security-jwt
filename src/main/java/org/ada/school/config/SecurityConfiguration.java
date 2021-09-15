045b68ecb935ecbd0bdcab20312d7ceb48433d0f
Juan Garcia <juan.garcia-ga@escuelaing.edu.co>
2021-09-14 23:07:45 -0500

    Primer parte

---
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package org.ada.school.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity( securedEnabled = true, jsr250Enabled = true, prePostEnabled = true )
+public class SecurityConfiguration
+        extends WebSecurityConfigurerAdapter
+{
+
+
+    @Override
+    protected void configure( HttpSecurity http )
+            throws Exception
+    {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers( HttpMethod.GET, "/v1/health" ).permitAll()
+                .antMatchers( HttpMethod.POST,"/v1/auth" ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS );
+    }
+}
+
