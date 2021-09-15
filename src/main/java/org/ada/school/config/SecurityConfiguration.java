f22b2f4069de76f966fcc19858822f54ac57a87d
Juan Garcia <juan.garcia-ga@escuelaing.edu.co>
2021-09-14 23:53:51 -0500

    Parte 2 terminada

--- src/main/java/org/ada/school/config/SecurityConfiguration.java
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -22,6 +22,7 @@ public class SecurityConfiguration
                 .authorizeRequests()
                 .antMatchers( HttpMethod.GET, "/v1/health" ).permitAll()
                 .antMatchers( HttpMethod.POST,"/v1/auth" ).permitAll()
+                .antMatchers( HttpMethod.POST,"/v1/user" ).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS );
